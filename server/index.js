a8cfeecda17b80b122d58abd8c038d119effde40
Charles <okoyecharles509@gmail.com>
2023-08-30 13:14:28 +0100

    Initialize server app

---
+++ server/index.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
+import colors from '@colors/colors/safe';
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+app.use(express.json({ limit: '30mb' }));
+app.use(express.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors({ origin: process.env.ORIGIN, credentials: true }));
+app.use(morgan('dev'));
+app.use(cookieParser());
+
+app.get('/', (_, res) => {
+  res.send('Welcome to your express application!');
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(colors.cyan("App started"))
+})
+
+// Dependencies
+// npm i @colors/colors bcryptjs cookie-parser cors dotenv express jsonwebtoken morgan
+// npm i -D nodemon
+
+// .env
+// ORIGIN = localhost:3000
+// PORT = 4000
